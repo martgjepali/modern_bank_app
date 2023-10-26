d12ab5e7b84ad1e67d1ec2da9ee4c1b70aaece21
Mart Gjepali <gjepalimart@gmail.com>
2023-10-26 15:18:19 +0200

    modern bank app

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,20 @@
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
+
+const CardDeal = () => {
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find better card deal <br className='sm:block hidden' /> in few easy steps</h2>
+        <p className={`${styles.paragraph} max-2-[470px] mt-5`}></p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]' />
+      </div>
+    </section>
+  )
+}
+
+export default CardDeal
